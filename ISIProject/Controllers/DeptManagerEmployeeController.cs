195e4961214f20bfe7c9f1e197849ec044444a27
ioana91 <ioa.fotea@gmail.com>
2014-01-03 18:54:25 +0200

    New controller for the Department Manager to be able to add employees to his department

---
+++ ISIProject/Controllers/DeptManagerEmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+using ISIProject.Models;
+
+namespace ISIProject.Controllers
+{
+    public class DeptManagerEmployeeController : Controller
+    {
+        private CompanyContext db = new CompanyContext();
+        //
+        // GET: /DeptManagerEmployee/
+
+        public ActionResult Index()
+        {
+            return View(db.Employees.Where(e => e.UserName != "admin").
+                Include("Department").ToList());
+        }
+
+    }
+}
