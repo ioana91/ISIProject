9a85be1662a83480eab26f563b6b5005c4abeee1
ioana91 <ioa.fotea@gmail.com>
2014-01-03 17:07:21 +0200

    Timesheet Controller created

---
+++ ISIProject/Controllers/TimesheetController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ISIProject.Controllers
+{
+    public class TimesheetController : Controller
+    {
+        //
+        // GET: /Timesheet/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
