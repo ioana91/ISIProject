ffd499475fc6564a9c8d5932c993c6260c8d8864
Tapus Radu <radutzp@yahoo.com>
2014-01-09 10:59:06 -0800

    Worked on TimeSheet

--- ISIProject/Controllers/TimesheetController.cs
+++ ISIProject/Controllers/TimesheetController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ISIProject.Models;
+using System.Globalization;
+using System.Web.Script.Serialization;
+
 
 namespace ISIProject.Controllers
 {
@@ -10,11 +14,111 @@ namespace ISIProject.Controllers
     {
         //
         // GET: /Timesheet/
+        private CompanyContext db = new CompanyContext();
 
         public ActionResult Index()
         {
             return View();
         }
 
+        [HttpPost]
+        public JsonResult GetEvents()
+        {
+            var loggedInUserName = User.Identity.Name;
+            int userID = db.Employees.Where(e => e.UserName == loggedInUserName).FirstOrDefault().EmployeeId;
+
+            //return Json(new
+            //{
+            //    id = "1",
+            //    start = new DateTime(2014, 1, 4, 14, 30, 0).ToString("MMMM dd, yyyy HH:mm:ss", new CultureInfo("en-US")),
+            //    end = new DateTime(2014, 1, 4, 16, 0, 0).ToString("MMMM dd, yyyy HH:mm:ss", new CultureInfo("en-US")),
+            //    title = "Super Test",
+            //});
+            var x = db.Timesheets.Where(tm => tm.EmployeeId == userID).ToList();
+
+            var y = x.Select(tm =>
+                        new
+                        {
+                            id = tm.TimesheetId,
+                            start = tm.StartTime.ToString("MMMM dd, yyyy HH:mm:ss", new CultureInfo("en-US")),
+                            end = tm.EndTime.ToString("MMMM dd, yyyy HH:mm:ss", new CultureInfo("en-US")),
+                            title = tm.Activity.Name,
+                        }).ToArray();
+
+            var json = Json(y);
+
+            return json;
+        }
+
+        [HttpPost]
+        public int AddEvent(CalendarEvent NewEvent)
+        {
+            var timeSheet = new Timesheet
+            {
+                StartTime = NewEvent.start,
+                EndTime = NewEvent.end,
+                Date = NewEvent.start.Date,
+                EmployeeId = db.Employees.FirstOrDefault(e => e.UserName == User.Identity.Name).EmployeeId,
+                ActivityId = 1,
+                ExtraHours = false,
+                State = TimesheetState.Open,
+            };
+
+            db.Timesheets.Add(timeSheet);            
+            db.SaveChanges();
+
+            //var json = Json(new {newId = timeSheet.TimesheetId});
+
+            return timeSheet.TimesheetId;
+        }
+
+        [HttpPost]
+        public void UpdateEvent(CalendarEvent NewEvent)
+        {
+            var timeSheet = db.Timesheets.Where(tm => tm.TimesheetId == NewEvent.id).FirstOrDefault();
+
+            timeSheet.StartTime = NewEvent.start;
+            timeSheet.EndTime = NewEvent.end;
+            timeSheet.Date = NewEvent.start.Date;
+            timeSheet.ActivityId = 1;
+            timeSheet.ExtraHours = false;
+            timeSheet.State = TimesheetState.Open;
+
+            //db.Timesheets.Add(timeSheet);
+            db.SaveChanges();
+        }
+
+        [HttpPost]
+        public void RemoveEvent(CalendarEvent NewEvent)
+        {
+            var timeSheet = db.Timesheets.Where(tm => tm.TimesheetId == NewEvent.id).FirstOrDefault();
+            db.Timesheets.Remove(timeSheet);
+            db.SaveChanges();
+        }
+
+        [HttpPost]
+        public JsonResult GetActivities(string projectID)
+        {
+           // List<Activity> activities;
+
+            if (projectID == "-1")
+            {
+                //activities = db.Activities.Select(new {})
+            }
+            else
+            {
+
+            }
+
+            var activities = db.Activities.Select(a =>
+                new
+                {
+                    a.ActivityId,
+                    a.Name
+                }).ToArray();
+
+
+            return Json(activities);
+        }
     }
 }
---
+++ ISIProject/Utils/LinkedDropdownHelper.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace ISIProject.Utils
+{
+    public static class LinkedDropdownHelper
+    {
+        #region Methods
+
+        public static MvcHtmlString LinkedDropdownListFor<TModel, TProperty>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TProperty>> expression, string parent, IEnumerable<LinkedSelectListItem> selectList, bool removedefault = false)
+        {
+            string propertyName = ((MemberExpression)expression.Body).Member.Name;
+
+            TagBuilder select = new TagBuilder("select");
+            select.Attributes.Add("id", propertyName);
+            select.Attributes.Add("name", propertyName);
+            //select.Attributes.Add("class", "linked-dropdown");
+            select.Attributes.Add("class", parent);
+
+            foreach (var item in selectList)
+            {
+                if (removedefault && item.Value == "-1")
+                {
+                    //skip default
+                }
+                else
+                {
+                    TagBuilder option = new TagBuilder("option");
+                    option.InnerHtml = item.Text;
+                    option.Attributes.Add("value", item.Value);
+                    option.Attributes.Add("class", item.LinkValue);
+
+                    if (item.Selected)
+                    {
+                        option.Attributes.Add("selected", "selected");
+                    }
+                    select.InnerHtml += option.ToString(TagRenderMode.Normal);
+                }
+            }
+            //below code was changed by abdurrauf to support jquery chains
+            string script = @"<script type='text/javascript'>$(document).bind('ready', function(){$(function(){
+                    $('#" + propertyName + "').chained('#" + parent + "');});});</script>";
+
+            return MvcHtmlString.Create(script + select.ToString(TagRenderMode.Normal));
+        }
+
+        #endregion Methods
+    }
+
+    public class LinkedSelectList : IEnumerable<LinkedSelectListItem>
+    {
+        #region Constructors
+
+        public LinkedSelectList(IEnumerable items, string dataValueField, string dataTextField, string dataLinkedValueField, IEnumerable selectedValues)
+        {
+            if (items == null)
+            {
+                throw new ArgumentNullException("items");
+            }
+
+            Items = items;
+            DataValueField = dataValueField;
+            DataTextField = dataTextField;
+            DataLinkedValueField = dataLinkedValueField;
+            SelectedValues = selectedValues;
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public string DataLinkedValueField
+        {
+            get;
+            private set;
+        }
+
+        public string DataTextField
+        {
+            get;
+            private set;
+        }
+
+        public string DataValueField
+        {
+            get;
+            private set;
+        }
+
+        public IEnumerable Items
+        {
+            get;
+            private set;
+        }
+
+        public IEnumerable SelectedValues
+        {
+            get;
+            private set;
+        }
+
+        #endregion Properties
+
+        #region Methods
+
+        public virtual IEnumerator<LinkedSelectListItem> GetEnumerator()
+        {
+            return GetListItems().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        internal IList<LinkedSelectListItem> GetListItems()
+        {
+            return GetListItemsWithValueField();
+        }
+
+        private static string Eval(object container, string expression)
+        {
+            object value = container;
+            if (!String.IsNullOrEmpty(expression))
+            {
+                value = DataBinder.Eval(container, expression);
+            }
+            return Convert.ToString(value, CultureInfo.CurrentCulture);
+        }
+
+        private IList<LinkedSelectListItem> GetListItemsWithValueField()
+        {
+            HashSet<string> selectedValues = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+            if (SelectedValues != null)
+            {
+                selectedValues.UnionWith(from object value in SelectedValues select Convert.ToString(value, CultureInfo.CurrentCulture));
+            }
+
+            var listItems = from object item in Items
+                            let value = Eval(item, DataValueField)
+                            select new LinkedSelectListItem
+                            {
+                                Value = value,
+                                Text = Eval(item, DataTextField),
+                                LinkValue = Eval(item, DataLinkedValueField),
+                                Selected = selectedValues.Contains(value)
+                            };
+            return listItems.ToList();
+        }
+
+        #endregion Methods
+    }
+
+    public class LinkedSelectListItem
+    {
+        #region Properties
+
+        public string LinkValue
+        {
+            get;
+            set;
+        }
+
+        public bool Selected
+        {
+            get;
+            set;
+        }
+
+        public string Text
+        {
+            get;
+            set;
+        }
+
+        public string Value
+        {
+            get;
+            set;
+        }
+
+        #endregion Properties
+    }
+}
